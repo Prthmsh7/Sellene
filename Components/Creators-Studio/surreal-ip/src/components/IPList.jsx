fe7c2907aa7cbc3007b958526c412d33afac3200
Prthmsh7 <Prathmeshshukla2106@gmail.com>
2025-06-06 00:03:55 +0530

    Added the structure for text, image and audio originality check using Yakoa

--- Components/Creators-Studio/surreal-ip/src/components/IPList.jsx
+++ Components/Creators-Studio/surreal-ip/src/components/IPList.jsx
@@ -1,16 +1,27 @@
 import React from 'react'
+import './IPList.css'
 
 export default function IPList({ ips }) {
   return (
-    <div style={{ margin: '24px 0', padding: 16, border: '1px solid #eee' }}>
+    <div className="ip-list">
       <h3>Registered IPs</h3>
       {ips.length === 0 && <div>No IPs registered yet.</div>}
       {ips.map((ip, idx) => (
-        <div key={idx} style={{ marginBottom: 12 }}>
-          <b>{ip.title}</b>
-          <div>{ip.description}</div>
-          <div style={{ fontSize: 12, color: 'gray' }}>
-            Owner: {ip.owner} | Date: {ip.date}
+        <div key={idx} className="ip-list-item">
+          {ip.type === 'image' ? (
+            <div>
+              <img src={ip.preview} alt={ip.title} className="ip-list-image" />
+              <div className="ip-score">Originality Score: {ip.score}%</div>
+            </div>
+          ) : (
+            <div>
+              <b>{ip.title}</b>
+              <div>{ip.description}</div>
+              <div className="ip-score">Originality Score: {ip.score}%</div>
+            </div>
+          )}
+          <div className="ip-meta">
+            Owner: {ip.owner} | Date: {ip.date} | Type: {ip.type}
           </div>
         </div>
       ))}
